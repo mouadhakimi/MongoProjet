0da72f253863249792bdfc2686b3552a53e212ff
mouadhakimi <mouad.hakimi@student.yncrea.fr>
2022-10-24 13:58:08 +0200

    Update main_client.py

--- main_client.py
+++ main_client.py
@@ -1,3 +1,13 @@
 import geocoder
+import pymongo
+
 g = geocoder.ip('me')
 print(g.latlng)
+
+
+myclient = pymongo.MongoClient("mongodb+srv://hakimimouad:admin@cluster0.mobs5qu.mongodb.net/test")
+mydb = myclient["vls"]
+mycol = mydb["stations"]
+
+for x in mycol.find():
+  print(x)
