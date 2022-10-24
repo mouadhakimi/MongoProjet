4a83aede7b95436b6ebd59b25c7516963c72b71d
mouadhakimi <mouad.hakimi@student.yncrea.fr>
2022-10-24 15:06:16 +0200

    Update main_client.py

--- main_client.py
+++ main_client.py
@@ -9,5 +9,16 @@ myclient = pymongo.MongoClient("mongodb+srv://hakimimouad:admin@cluster0.mobs5qu
 mydb = myclient["vls"]
 mycol = mydb["stations"]
 
-for x in mycol.find():
-  print(x)
+x = 50.634178
+y = 3.048710
+
+
+mycolcursor = mydb.stations.find({},{ "_id": 0, "name": 1, "geometry.coordinates": 1 })
+
+for i in mycolcursor:
+  z = i.get("geometry")
+  w = z.get("coordinates")
+  if(w[0]<y+0.003 and w[0]>y-0.003):
+      if(w[1]<x+0.003 and w[1]>x-0.003):
+          print(i)
+
